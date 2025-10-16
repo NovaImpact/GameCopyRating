9665375f614df3ea585c941149b810280676229d
adilk <adilkabir738@gmail.com>
2025-10-16 09:20:45 -0400

    Update 1

--- src/GameCopiesSold.java
+++ src/GameCopiesSold.java
@@ -1,4 +1,20 @@
+import java.time.LocalDate;
+
 public class GameCopiesSold extends VideoGames {
 
     int copiesSold;
+
+    public GameCopiesSold(int rank, String title, LocalDate releaseDate, boolean multiPlatform, int copiesSold) {
+        super(rank, title, releaseDate, multiPlatform);
+        this.copiesSold = copiesSold;
+    }
+
+    public int getCopiesSold() {
+        return copiesSold;
+    }
+
+    public void setCopiesSold(int copiesSold) {
+        this.copiesSold = copiesSold;
+    }
+
 }
