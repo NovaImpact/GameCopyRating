bd7668e05094cc6016f90b8f25dd467021ae266d
Sam <zliu26@scienceleadership.org>
2025-10-17 09:33:47 -0400

    m

--- src/GameRating.java
+++ src/GameRating.java
@@ -1,4 +1,10 @@
+import java.time.LocalDate;
+
 public class GameRating extends VideoGames {
     int metaScore;
     boolean RatedE;
+
+    public GameRating(int rank, String title, LocalDate releaseDate, boolean multiPlatform) {
+        super(rank, title, releaseDate, multiPlatform);
+    }
 }
