24309bdcab237b914dc2ef11a34fbbb2ef94050d
sam <zliu26@scienceleadership.org>
2025-10-24 10:33:53 -0400

    aa

--- src/GameRating.java
+++ src/GameRating.java
@@ -1,7 +1,5 @@
 import java.io.File;
-import java.lang.reflect.Array;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GameRating extends VideoGames {
@@ -12,51 +10,48 @@ public class GameRating extends VideoGames {
         super(rank, title, releaseDate, multiPlatform);
     }
 
+
     public String toString() {
         //String
         return super.toString();
     }
 
     public static void readGameRatingData() throws Exception {
-        GameRating.readGameRatingData();
+        //GameRatingData.readGameRatingData();
 
-        File myData = new File("GameRating");
+        File myData = new File("GameRatingData");
         Scanner myReader = new Scanner(myData);
+        String firstLine = myReader.nextLine();
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
             Scanner lineScanner = new Scanner(data);
             lineScanner.useDelimiter("\t");
-            // System.out.println(data);
+            System.out.println(data);
+
+            //must-play
+            //1. The Legend of Zelda: Ocarina of Time
+            //Nov 23, 1998  •  Rated E
+            //As a young boy, Link is tricked by Ganondorf, the King of the Gerudo Thieves. The evil human uses Link to gain access to the Sacred Realm, where he places his tainted hands on Triforce and transforms the beautiful Hyrulean landscape into a barren wasteland. Link is determined to fix the problems he helped to create, so with the help of Rauru he travels through time gathering the powers of the Seven Sages.
+            //99
+            //Metascore
 
             String copyChunk = lineScanner.next();
             copyChunk = copyChunk.replace("M", "");
 
-            LocalDate game1LocalDate = LocalDate.of(2011, 11, 18);
+            //LocalDate game1Date = LocalDate.of(2011, 11, 18);
 
             int rank = Integer.parseInt(copyChunk);
-
             String title = lineScanner.next();
-            int copiesSold = lineScanner.nextInt();
             boolean multiPlatform = lineScanner.nextBoolean();
 
+            LocalDate game1RatingDate = LocalDate.of(1998, 11, 23);
+            new GameRating(rank, title, game1RatingDate, multiPlatform);
 
         }
 
-        //must-play
-        //1. The Legend of Zelda: Ocarina of Time
-        //Nov 23, 1998  •  Rated E
-        //As a young boy, Link is tricked by Ganondorf, the King of the Gerudo Thieves. The evil human uses Link to gain access to the Sacred Realm, where he places his tainted hands on Triforce and transforms the beautiful Hyrulean landscape into a barren wasteland. Link is determined to fix the problems he helped to create, so with the help of Rauru he travels through time gathering the powers of the Seven Sages.
-        //99
-        //Metascore
 
-        for (int i = 0; i < 0; i = i + 1) {
-            int rank = 1;
-            String title = "The Legend of Zelda: Ocarina of Time";
-            int year = 1998;
-            LocalDate film1LocalDate = LocalDate.of(year, 11, 23);
-            boolean multiPlatform = false;
 
-        }
+
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,12 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
 
-
-import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.util.Locale;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        LocalDate game1Date = LocalDate.of(1998, 11, 23);
-        new GameRating(1, "The Legend of Zelda: Ocarina of Time", game1Date, false);
+        GameRating.readGameRatingData();
 
-        LocalDate game2Date = LocalDate.of(1999, 9, 8);
-        new GameRating(2,"SoulCalibur", game2Date,true);
-       // GameRating game2 = new GameRating(2,"SoulCalibur", game2Date,true);
-
-
-        LocalDate game1LocalDate = LocalDate.of(2011, 11, 18);
-        new GameCopiesSold(1, "Minecraft", game1Date,true,350,000,000);
-        game2Date = LocalDate.of(2013, 9, 17);
-        new GameCopiesSold(2, "Grand Theft Auto V", game2Date,true,215,000,000);
-
-
-       // System.out.println(VideoGames.getAllVideoGames());
-
+    System.out.println("Game Ratings Only: ");
     for (VideoGames eachCopy : VideoGames.getAllVideoGames()) {
             System.out.println(eachCopy);
         }
-
     }
 }
