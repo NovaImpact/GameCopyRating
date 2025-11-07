4df9d6e43c7c2774cded798b5638cf6b97200c94
adilk <adilkabir738@gmail.com>
2025-11-06 19:45:41 -0500

    Update 11

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,11 @@
+import java.time.LocalDate; import java.util.ArrayList;
 import java.time.LocalDate;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        GameRating.readGameRatingData();
-
-    System.out.println("Game Ratings Only: ");
-    for (VideoGames eachCopy : VideoGames.getAllVideoGames()) {
-        LocalDate game1Date = LocalDate.of(1998, 11, 23);
-        new GameRating(1, "The Legend of Zelda: Ocarina of Time", game1Date, false);
-
-        GameCopiesSold.readGameCopiesData();
-
-        for (VideoGames eachCopy : VideoGames.getAllVideoGames()) {
-            System.out.println(eachCopy);
-
+import java.time.chrono.ChronoLocalDate;
+import java.util.Locale;
+public class Main { public static void main(String[] args) throws Exception
+    { LocalDate game1Date = LocalDate.of(1998, 11, 23); new GameRating(1, "The Legend of Zelda: Ocarina of Time", game1Date, false);
+        GameCopiesSold.readGameCopiesData(); for (VideoGames eachCopy : VideoGames.getAllVideoGames())
+        { System.out.println(eachCopy);
         }
     }
-    }
-
 }
